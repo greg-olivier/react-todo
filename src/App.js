dfff57cf2598e70e8ea25e4ebbce64b0da9ac404
Greg <greg.olivier87@gmail.com>
2018-08-31 11:07:20 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import classes from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+        <header className={classes.header}>
+          <img src={logo} className={classes.logo} alt="logo" />
+          <h1 className={classes.title}>Welcome to React</h1>
+        </header>
+        <p className={classes.intro}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
